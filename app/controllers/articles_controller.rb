99e806537d44b150b8c764c6b4564243d3b79538
Sarath <sarathn2013@gmail.com>
2016-02-13 00:05:20 -0600

    create and view pages

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:update, :destroy, :show, :edit]
+def index
+	@articles=Article.all
+end
+def new
+	@article = Article.new
+end
+
+def create
+	@article = Article.new(article_params)
+	if @article.save
+		redirect_to root_path
+	else
+		render 'new'
+	end
+end
+
+def show
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+private
+def article_params
+	params.require(:article).permit(:title, :description)
+end
+
+def find_article
+	@article=Article.find(params[:id])
+end
+end
