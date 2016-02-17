25a71ec36629a5a0c370ab7e8f88ccfc8651a0b8
Sarath <sarathn2013@gmail.com>
2016-02-17 09:11:41 -0600

    done with articles index styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, length: {minimum: 3,maximum: 10} 
-	validates :description, presence: true, length: {minimum: 8, maximum: 40}
+	validates :title, presence: true, length: {minimum: 3,maximum: 30} 
+	validates :description, presence: true, length: {minimum: 8, maximum: 100}
 end
