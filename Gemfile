7f3abead4379d493ee3edc02b91b0e7190196659
Sarath <sarathn2013@gmail.com>
2016-02-18 12:24:08 -0600

    add secure password

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 ruby '2.2.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
+
+gem 'bcrypt', '~> 3.1.7'
 # Use sqlite3 as the database for Active Record
 gem 'bootstrap-sass', '~> 3.3.6'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218162330) do
+ActiveRecord::Schema.define(version: 20160218181822) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160218162330) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
