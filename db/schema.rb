b743be16ad09612a610e255a31916ad109f8caea
Sarath <sarathn2013@gmail.com>
2016-02-18 10:49:52 -0600

    added relation between articles and users and added debugging

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218013415) do
+ActiveRecord::Schema.define(version: 20160218162330) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "pages", force: :cascade do |t|
