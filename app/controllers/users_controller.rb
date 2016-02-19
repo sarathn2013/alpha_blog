5e08ffb81cfe188c0db8915a54ad9e71c1c883b7
Sarath <sarathn2013@gmail.com>
2016-02-19 10:50:44 -0600

    user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-   before_action :find_user, only: [:edit, :update]
+   before_action :find_user, only: [:edit, :update, :show]
 def new
 	@user = User.new
 end
@@ -7,7 +7,7 @@ end
 def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Welcome to alpha blog #{@user.username}'
+      flash[:success] = "Welcome to alpha blog #{@user.username}"
       redirect_to articles_path
 
     else
@@ -17,6 +17,9 @@ end
 
 def edit
 
+end
+
+def show
 end
 
 def update
