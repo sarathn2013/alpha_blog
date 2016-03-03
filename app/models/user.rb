d1ec570ff630da31b01114a216d2bdcf5ae3880a
Sarath <sarathn2013@gmail.com>
2016-03-03 12:01:13 -0600

    changed password format

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,13 @@ class User < ActiveRecord::Base
 	           length: {maximum: 105}, 
 	           format: {with: VALID_EMAIL_REGEX}
 	has_secure_password
-	PASSWORD_FORMAT = /\A
-  (?=.{8,})          # Must contain 8 or more characters
-  (?=.*\d)           # Must contain a digit
-  (?=.*[a-z])        # Must contain a lower case character
-  (?=.*[A-Z])        # Must contain an upper case character
-  (?=.*[[:^alnum:]]) # Must contain a symbol
-/x
-	validates :password, length: {minimum: 8, maximum: 20}, format: {with: PASSWORD_FORMAT}
+	
+	#PASSWORD_FORMAT = /\A
+  #(?=.{8,})          # Must contain 8 or more characters
+  #(?=.*\d)           # Must contain a digit
+  #(?=.*[a-z])        # Must contain a lower case character
+  #(?=.*[A-Z])        # Must contain an upper case character
+ # (?=.*[[:^alnum:]]) # Must contain a symbol
+#/x
+	validates :password, length: {minimum: 8, maximum: 20}#, format: {with: PASSWORD_FORMAT}
 end
